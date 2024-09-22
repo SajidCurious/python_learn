58ffbfb6c8f59c51ca0f8b1d8fb7d8424d0d53a1
Mohammed <sajidcurious9@gmail.com>
2024-09-21 23:44:17 -0700

    made a simple program to check the number is multiple of 5 or not

---
+++ conditional_operators/q3.py
@@ -0,0 +1,7 @@
+# Take an input number from the user and check whether it is the multiple of 5
+
+number = int(input("Enter the number: "))
+if(number % 5 == 0):
+    print("Yes the number is multiple of 5")
+else:
+    print("The number is not the multiple of 5")
