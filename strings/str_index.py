495f3d45fcb6c627d908a78631802f1ec677dcfe
Mohammed <sajidcurious9@gmail.com>
2024-09-21 17:52:12 -0700

    renamed them

--- strings/str_index.py
+++
@@ -1,3 +0,0 @@
-college = "SFBU"
-find_index = college[1]
-print(find_index)
--- strings/str_length.py
+++
@@ -1,5 +0,0 @@
-# Joing the two or more strings
-name1 = "Mohammed"
-name2 = "Sajid"
-Full_name = name1 + name2
-print(len(Full_name))
