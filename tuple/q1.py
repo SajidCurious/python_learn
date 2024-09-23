08128cbe8300205f9b465a3bb4eedc11d43ae3d7
Mohammed <sajidcurious9@gmail.com>
2024-09-22 22:04:25 -0700

    wrote a program to ask the user to enter names of their 3 favourite movies and store them in a list

---
+++ tuple/q1.py
@@ -0,0 +1,8 @@
+# Write a program to ask the user to enter names of their 3 favourite movies and store them in a list.
+
+a = input("Enter your favourite movie 1: ")
+b = input("Enter your favourite movie 2: ")
+c = input("Enter your favourite movie 3: ")
+
+list = [a,b,c]
+print(list)
