89137ec998929986397c3c6b6ac69ea168b31014
Mohammed <sajidcurious9@gmail.com>
2024-09-22 17:20:47 -0700

    added second method to sort list in descending order

--- lists/sort.py
+++ lists/sort.py
@@ -1,5 +1,10 @@
-# Sort method in Lists is used to align the data 
+# Sort method in Lists is used to align the data in ascending order
 
 cost = [25,60,15,10,93,5]
 cost.sort()
 print(cost)
+
+# Sort method in Lists is used to align the data in Descending order
+cost1 = [60,40,5,3,9,25,19,55,42,34]
+cost1.sort(reverse=True)
+print(cost1)
