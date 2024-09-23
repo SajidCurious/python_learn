866ff25329cc53022265b8fca72d795c8b47062e
Mohammed <sajidcurious9@gmail.com>
2024-09-22 22:11:06 -0700

    solved the problem in a second way

--- tuple/q1.py
+++ tuple/q1.py
@@ -1,8 +1,18 @@
 # Write a program to ask the user to enter names of their 3 favourite movies and store them in a list.
 
-a = input("Enter your favourite movie 1: ")
-b = input("Enter your favourite movie 2: ")
-c = input("Enter your favourite movie 3: ")
+# a = input("Enter your favourite movie 1: ")
+# b = input("Enter your favourite movie 2: ")
+# c = input("Enter your favourite movie 3: ")
 
-list = [a,b,c]
-print(list)
+# list = [a,b,c]
+# print(list)
+
+movies = []
+movie1 = input("Enter your favourite movie 1: ")
+movie2 = input("Enter your favourite movie 2: ")
+movie3 = input("Enter your favourite movie 3: ")
+
+movies.append(movie1)
+movies.append(movie2)
+movies.append(movie3)
+print(movies)
