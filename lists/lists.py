05381a57f42e10e6f1a57b7bb039f3e921ad1576
Mohammed <sajidcurious9@gmail.com>
2024-09-22 17:09:00 -0700

    added lists folder

---
+++ lists/lists.py
@@ -0,0 +1,6 @@
+# List is similar to the arrays in other programming languages but with minimal differences
+
+# Any type of the data structure can be stored in a list.
+
+Features = ["BMW","Black",20000,200.99]
+print(Features)
