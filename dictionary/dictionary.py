da0aa3fbf09f01ce795ecae161a38dbca638e8d9
Mohammed <sajidcurious9@gmail.com>
2024-09-24 16:41:40 -0700

    added example for the dictionary

---
+++ dictionary/dictionary.py
@@ -0,0 +1,9 @@
+# dictionary is a built-in data type in python which is used to store the multiple values using key:value pair format.
+
+student = {
+    "name" : "KKK",
+    "study" : "Graduate",
+    "id" : 20299,
+    "grade" : 3.2
+
+}
