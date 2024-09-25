736f0d6d1bc9e38147d1f863cfc990be0ccca8e6
Mohammed <sajidcurious9@gmail.com>
2024-09-24 18:22:14 -0700

    added the update method for the dictionary

---
+++ dictionary/update.py
@@ -0,0 +1,11 @@
+# This method is used to modify or add the key:values to the existing dictionary
+
+student = {
+    "name" : "KKK",
+    "study" : "Graduate",
+    "id" : 20299,
+    "grade" : 3.2
+
+}
+student.update({"city" : "Hyderabad"})
+print(student)
