d6df9f2015057a4dac51e0d16ff1421e159bc953
Mohammed <sajidcurious9@gmail.com>
2024-09-24 18:46:36 -0700

    added set folder and an example with it

---
+++ set/set.py
@@ -0,0 +1,4 @@
+# In Python, a set is an unordered collection of unique elements, which means each element appears only once. Sets are defined using the set() function or curly braces {}.
+
+sam = {1,2,3,4,5,6}
+print(sam)
