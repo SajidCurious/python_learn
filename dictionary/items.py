cb166c9392142e8f93fc60f3564cd85469e2fbf7
Mohammed <sajidcurious9@gmail.com>
2024-09-24 18:12:10 -0700

    added an example to return all the key:value pairs of a dictionary as a tuples

---
+++ dictionary/items.py
@@ -0,0 +1,10 @@
+# This method returns all the key:value pairs as tuples
+
+student = {
+    "name" : "KKK",
+    "study" : "Graduate",
+    "id" : 20299,
+    "grade" : 3.2
+
+}
+print(student.items())
