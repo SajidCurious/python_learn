de09f83eb4dedc9e89fcb5bf45db19c39a958e4f
Mohammed <sajidcurious9@gmail.com>
2024-09-24 18:08:45 -0700

    added an example to print all the values in a dictionary

---
+++ dictionary/values.py
@@ -0,0 +1,10 @@
+# This method returns all the values in a dictionary
+
+student = {
+    "name" : "KKK",
+    "study" : "Graduate",
+    "id" : 20299,
+    "grade" : 3.2
+
+}
+print(student.values())
