633e4aad4c72e8da9ac97a9a542860119d91f06b
Mohammed <sajidcurious9@gmail.com>
2024-10-01 19:13:15 -0700

    modified

--- for_loop/for_loop.py
+++ for_loop/for_loop.py
@@ -1,1 +1,5 @@
-# For loops are used for sequential travel. For traversing Lists,
+# For loops are used for sequential travel. For traversing Lists,Strings and Tuples.
+
+tup = [1,2,3,4,5,6,7,8,9,10]
+for num in tup:
+    print(num)
