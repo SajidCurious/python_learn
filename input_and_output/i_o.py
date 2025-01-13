7fb2b02da9a5740a46d80bb6a6fb1cde19687d77
Mohammed <sajidcurious9@gmail.com>
2025-01-12 20:58:19 -0800

    added simple example for the read information of the file

---
+++ input_and_output/i_o.py
@@ -0,0 +1,13 @@
+# Input
+# Input refers to data provided to a Python program, typically by the user, which the program processes. In Python, the input() function is used to accept input from the user.
+# The input() function reads data as a string. To work with other data types, the input needs to be converted (e.g., using int() or float()).
+
+# Output
+# Output refers to the data or information that a Python program produces and displays to the user or writes to a file. The print() function is commonly used for output in Python.
+# Output can also be formatted for readability using techniques like f-strings or .format().
+
+f = open("sajid.txt","r")
+data = f.read()
+print(data)
+print(type(data))
+f.close()
