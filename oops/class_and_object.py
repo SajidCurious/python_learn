1a231d8495b58688a86fe527ec66f21229541d21
Mohammed <sajidcurious9@gmail.com>
2025-01-16 20:55:14 -0800

    added the common college name for both of the objects

--- oops/class_and_object.py
+++ oops/class_and_object.py
@@ -2,12 +2,14 @@
 # object is an instance of the class
 
 class Student:
-    def __init__(self, fullname):
-        self.name = fullname
+    college = "SFBU"
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
         print("I'm winning the game")
 
-s1 = Student("Sajid")
-print(s1.name)
+s1 = Student("Sajid", 90)
+print(s1.name, s1.marks, s1.college)
 
-s2 = Student("Wajid")
-print(s2.name)
+s2 = Student("Wajid", 50)
+print(s2.name, s2.marks, s2.college)
