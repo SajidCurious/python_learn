43d4a0602ba4c6e37b708b1c61c8e78389ed93c2
Mohammed <sajidcurious9@gmail.com>
2025-01-16 20:17:51 -0800

    added simple class and object example

---
+++ oops/class_and_object.py
@@ -0,0 +1,10 @@
+# class is a blueprint for the object
+# object is an instance of the class
+
+class Student:
+    name = "Sajid"
+    def __init__(self):
+        print("I'm winning the game")
+
+s1 = Student()
+# print(s1.name)
